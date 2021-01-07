3da95a696aa630470a96df2584c780ebd120e603
Michael <mjryan110@gmail.com>
2021-01-06 18:49:56 -0600

    adding checkForName.js

---
+++ src/client/js/checkForName.js
@@ -0,0 +1,12 @@
+function checkForName(inputURL) {
+    var regex = inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+
+    if(regex == mull){
+        return 0;
+    } else {
+        return 1;
+    }
+
+}
+
+export { checkForName }
