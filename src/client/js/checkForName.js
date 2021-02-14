17989bfead089b5b99eae08f2440abf816fa3ac4
Michael <mjryan110@gmail.com>
2021-02-14 13:45:45 -0600

    update

--- src/client/js/checkForName.js
+++ src/client/js/checkForName.js
@@ -1,7 +1,12 @@
 function checkForName(inputURL) {
-    var regex = inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    let pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
 
-    if(regex == null){
+    if(pattern == null){
         return false;
     } else {
         return true;
