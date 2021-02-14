2948c456ecd559c5b1846ce1118a2f57df8cccc7
Michael <mjryan110@gmail.com>
2021-02-13 21:09:15 -0600

    update

--- src/client/js/checkForName.js
+++ src/client/js/checkForName.js
@@ -1,11 +1,11 @@
 function checkForName(inputURL) {
-    var regex = inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-
-    if(regex == null){
-        return 0;
-    } else {
-        return 1;
+    const regex =  /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gi;
+    if (regex.test(inputURL)) {
+        return true;
     }
+
+    return false;
+
 
 }
 
